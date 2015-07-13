801893e3bb6e6559cb2c51302c8adb368defde08
irinapop <irinapop@EVO206.evointern>
2015-07-13 12:31:52 +0300

    organize imports

--- src/main/java/com/pages/LogInPage.java
+++ src/main/java/com/pages/LogInPage.java
@@ -1,20 +1,8 @@
 package com.pages;
 
-import ch.lambdaj.function.convert.Converter;
-import net.thucydides.core.annotations.DefaultUrl;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import net.serenitybdd.core.pages.WebElementFacade;
-
 import net.serenitybdd.core.annotations.findby.FindBy;
-
+import net.serenitybdd.core.pages.WebElementFacade;
 import net.thucydides.core.pages.PageObject;
-
-import java.util.List;
-
-import static ch.lambdaj.Lambda.convert;
 
 
 public class LogInPage extends PageObject {
