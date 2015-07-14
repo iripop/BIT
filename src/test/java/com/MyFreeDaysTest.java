132b0118157bd32730248251a7e744b5f585d940
biancaianc <biancaianc@EVO235.evointern>
2015-07-14 17:17:04 +0300

    selected business days are correct

--- src/test/java/com/MyFreeDaysTest.java
+++ src/test/java/com/MyFreeDaysTest.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 
 import com.steps.EndUserSteps;
+import com.steps.NewVacationRequestsSteps;
 import com.steps.NumberOfFreeDaysSteps;
 
 import net.serenitybdd.junit.runners.SerenityParameterizedRunner;
@@ -31,12 +32,21 @@ public class MyFreeDaysTest {
     public NumberOfFreeDaysSteps myFreeDay;
     @Steps
     public EndUserSteps endUser;
-    @Test
+    @Steps
+    public NewVacationRequestsSteps newVacationUser;
+  //  @Test
     public void my_free_days() {
         endUser.is_the_home_page();
 		endUser.login(username, password);
 		endUser.go_to_vacation_menu();
-		myFreeDay.my_free_days_is_correct();
+		myFreeDay.my_free_days_are_correct();
     }
-  
+    @Test
+    public void selected_business_days(){
+    	endUser.is_the_home_page();
+		endUser.login(username, password);
+		endUser.go_to_vacation_menu();
+		newVacationUser.go_to_new_vacation_request_page();
+		myFreeDay.selected_business_days_are_correct(14, "August", 2015, 17, "August", 2015);
+    }
 } 
--- src/test/java/com/steps/NumberOfFreeDaysSteps.java
+++ src/test/java/com/steps/NumberOfFreeDaysSteps.java
@@ -2,12 +2,15 @@ package com.steps;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 import org.openqa.selenium.WebElement;
 
 import com.pages.LogInPage;
 import com.pages.MyFreeDaysPage;
 import com.pages.MyRequestsPage;
-
+import com.pages.NewVacationRequestPage;
 
 import net.thucydides.core.annotations.Step;
 import net.thucydides.core.annotations.StepGroup;
@@ -22,9 +25,32 @@ public class NumberOfFreeDaysSteps extends ScenarioSteps {
 
 	
 	MyFreeDaysPage myFreeDaysPage;
+	NewVacationRequestPage newVacationPage;
     @Step
-    public void my_free_days_is_correct(){
-    	boolean isEqual = myFreeDaysPage.my_free_days_from_new_vacation_request_is_equal_with_my_free_days_from_my_free_days_menu();
+    public void my_free_days_are_correct(){
+    	boolean isEqual = myFreeDaysPage.my_free_days_from_new_vacation_request_are_equal_with_my_free_days_from_my_free_days_menu();
     	assertTrue("Is not equal",isEqual);
     }
+  //  @Step
+    public long dateDiffBetweenStartDateAndEndDate(int startDay,String startMonth,int startYear,int endDay,String endMonth,int endYear){
+    	int selectedDays=0;
+    	newVacationPage.choose_the_start_date(startDay, startMonth, endYear);
+    	newVacationPage.choose_the_end_date(endDay, endMonth, endYear);
+    	Date start = new GregorianCalendar(startYear, myFreeDaysPage.getMonth(startMonth), startDay).getTime();
+    	Date end=new GregorianCalendar(endYear, myFreeDaysPage.getMonth(endMonth), endDay).getTime();
+    	long diff = start.getTime()-end.getTime();
+
+    	return diff;
+    }
+    @Step
+    public void selected_business_days_are_correct(int startDay,String startMonth,int startYear,int endDay,String endMonth,int endYear){
+    	boolean isEqual = false;
+    	System.out.println(myFreeDaysPage.getSelectedBusinessDays());
+    	System.out.println(dateDiffBetweenStartDateAndEndDate(startDay, startMonth, startYear, endDay, endMonth, endYear));
+    	if(dateDiffBetweenStartDateAndEndDate(startDay, startMonth, startYear, endDay, endMonth, endYear)==myFreeDaysPage.getSelectedBusinessDays()) isEqual = true;
+    	else isEqual = false;
+    	assertTrue("Is not equal",isEqual);
+    	
+    }
+    
 }
