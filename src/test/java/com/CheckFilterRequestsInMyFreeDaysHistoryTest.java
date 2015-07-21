d34b41bdcd2a1211f5e233fc31e32f499aa5dd60
irinapop <irinapop@EVO206.evointern>
2015-07-21 10:27:44 +0300

    latest version

--- src/test/java/com/CheckFilterRequestsInMyFreeDaysHistoryTest.java
+++ src/test/java/com/CheckFilterRequestsInMyFreeDaysHistoryTest.java
@@ -16,6 +16,7 @@ import net.thucydides.core.annotations.ManagedPages;
 import net.thucydides.core.annotations.Steps;
 import net.thucydides.core.pages.Pages;
 import tools.Constants;
+
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @RunWith(SerenityRunner.class)
 public class CheckFilterRequestsInMyFreeDaysHistoryTest {
@@ -29,32 +30,21 @@ public class CheckFilterRequestsInMyFreeDaysHistoryTest {
 	public EndUserSteps endUser;
 	@Steps
 	public MyFreeDaysHistorySteps freeDaysHistorySteps;
-	
-	//@Test
-		public void enter_my_requests() {
-			endUser.openHomePage();
-	    	endUser.logInAsUser(Constants.DMname, Constants.DMpassword);
-	    	endUser.goToVacationMenu();
-			freeDaysHistorySteps.accessMyFreeDaysHistory();
-		}
-		
-		
 
-		@Test
-		public void check_if_filter_is_correct() {
-			endUser.openHomePage();
-	    	endUser.logInAsUser(Constants.DMname, Constants.DMpassword);
-	    	endUser.goToVacationMenu();
-			freeDaysHistorySteps.accessMyFreeDaysHistory();
-			freeDaysHistorySteps.filterRequestsInMyFreeDaysHistory("Vacation days");
-			freeDaysHistorySteps.filterRequestsInMyFreeDaysHistory("1 - 5");
-			freeDaysHistorySteps.filterRequestsInMyFreeDaysHistory("Added Days");
-			freeDaysHistorySteps.clickApplyButtonForFiltersInMyFreeDaysHistory();
-			freeDaysHistorySteps.checkFilteringRequestsByTypeInMyFreeDaysHistory("Vacation days");
-			freeDaysHistorySteps.checkFilteringRequestsByDaysNumberInMyFreeDaysHistory(1, 5);
-			freeDaysHistorySteps.checkFilteringRequestsByOperationInMyFreeDaysHistory("Added Days");
-			
+	@Test
+	public void check_if_filter_is_correct() {
+		endUser.openHomePage();
+		endUser.logInAsUser(Constants.DMname, Constants.DMpassword);
+		endUser.goToVacationMenu();
+		freeDaysHistorySteps.accessMyFreeDaysHistory();
+		freeDaysHistorySteps.filterRequestsInMyFreeDaysHistory("Vacation days");
+		freeDaysHistorySteps.filterRequestsInMyFreeDaysHistory("1 - 5");
+		freeDaysHistorySteps.filterRequestsInMyFreeDaysHistory("Added Days");
+		freeDaysHistorySteps.clickApplyButtonForFiltersInMyFreeDaysHistory();
+		freeDaysHistorySteps.checkFilteringRequestsByTypeInMyFreeDaysHistory("Vacation days");
+		freeDaysHistorySteps.checkFilteringRequestsByDaysNumberInMyFreeDaysHistory(1, 5);
+		freeDaysHistorySteps.checkFilteringRequestsByOperationInMyFreeDaysHistory("Added Days");
 
-		}
+	}
 
 }
